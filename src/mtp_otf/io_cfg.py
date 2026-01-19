c9c214c0d8f21653daeec87bcfbd85fbc823003a
Eugène Sanscartier <eugene.sanscartier@gmail.com>
2026-01-19 14:07:16 +0800

    trying something different for because of OSError: [Errno 14] Bad address

--- src/mtp_otf/io_cfg.py
+++ src/mtp_otf/io_cfg.py
@@ -6,7 +6,6 @@ def read_cfg(fileobj):
     lines = fileobj.readlines()
     images = []
     while len(lines) > 0:
-
         line = lines.pop(0)
         if line.strip() == "BEGIN_CFG":
             size = 0
@@ -65,6 +64,77 @@ def read_cfg(fileobj):
 
     return images
 
+# def write_cfg(fileobj, images, fmt='%12.6f'):
+#     def map2ranks(arr):
+#         types = []
+#         _none = [types.append(type) for type in arr if type not in types]
+#         rank_map = {val: i for i, val in enumerate(types)}
+#         return [rank_map[num] for num in arr]
+
+#     for atoms in images:
+#         fileobj.write("BEGIN_CFG\n")
+#         fileobj.write(" Size\n")
+#         fileobj.write("%9d\n" % len(atoms))
+#         fileobj.write(" Supercell\n")
+#         fileobj.write("    {} {} {}\n".format(fmt % atoms.get_cell()[0][0], fmt % atoms.get_cell()[0][1], fmt % atoms.get_cell()[0][2]))
+#         fileobj.write("    {} {} {}\n".format(fmt % atoms.get_cell()[1][0], fmt % atoms.get_cell()[1][1], fmt % atoms.get_cell()[1][2]))
+#         fileobj.write("    {} {} {}\n".format(fmt % atoms.get_cell()[2][0], fmt % atoms.get_cell()[2][1], fmt % atoms.get_cell()[2][2]))
+
+#         fields, fields_data = ["id", "type", "cartes_x", "cartes_y", "cartes_z"], {"id":[], "type":[], "cartes_x":[], "cartes_y":[], "cartes_z":[]}
+#         if atoms.calc != None and "forces" in atoms.calc.results:
+#             fields += ["fx", "fy", "fz"]
+#             fields_data["fx"], fields_data["fy"], fields_data["fz"] = [], [], []
+#         if "nbh_grades" in atoms.arrays:
+#             fields += ["nbh_grades"]
+#             fields_data["nbh_grades"] = []
+
+#         for i in range(len(atoms)):
+#             fields_data["id"] += [i+1]
+#             fields_data["type"] += [map2ranks(atoms.get_atomic_numbers())[i]]
+#             fields_data["cartes_x"] += [atoms.get_positions()[i][0]]
+#             fields_data["cartes_y"] += [atoms.get_positions()[i][1]]
+#             fields_data["cartes_z"] += [atoms.get_positions()[i][2]]
+#             if atoms.calc != None and "forces" in atoms.calc.results:
+#                 fields_data["fx"] += [atoms.calc.results["forces"][i][0]]
+#                 fields_data["fy"] += [atoms.calc.results["forces"][i][1]]
+#                 fields_data["fz"] += [atoms.calc.results["forces"][i][2]]
+#             if "nbh_grades" in atoms.arrays:
+#                 fields_data["nbh_grades"] += [atoms.arrays["nbh_grades"][i]]
+
+#         if "nbh_grades" in atoms.arrays:
+#             if "features" in atoms.info:
+#                 atoms.info["features"]["MV_grade"] = numpy.max(atoms.arrays["nbh_grades"])
+#             else:
+#                 atoms.info["features"] = {"MV_grade": numpy.max(atoms.arrays["nbh_grades"])}
+
+#         fileobj.write(" AtomData:  " + "    ".join(fields) + "\n")
+#         for i in range(len(atoms)):
+#             fileobj.write(" {:9d} {:3d} {} {} {} ".format(fields_data["id"][i], fields_data["type"][i], fmt % fields_data["cartes_x"][i], fmt % fields_data["cartes_y"][i], fmt % fields_data["cartes_z"][i]))
+#             if atoms.calc != None and "forces" in atoms.calc.results:
+#                 fileobj.write(" {} {} {} ".format(fmt % fields_data["fx"][i], fmt % fields_data["fy"][i], fmt % fields_data["fz"][i]))
+#             if "nbh_grades" in atoms.arrays:
+#                 fileobj.write(" {}".format(fmt % fields_data["nbh_grades"][i]))
+#             fileobj.write("\n")
+
+#         fileobj.write(" Energy\n")
+#         if atoms.calc != None and "energy" in atoms.calc.results:
+#             fileobj.write("    {}\n".format(fmt % atoms.calc.results["energy"]))
+#         else:
+#             fileobj.write("    {}\n".format(fmt % 0.0))
+
+#         if atoms.calc != None and "stress" in atoms.calc.results:
+#             stress_fields, stress_fields_data = ["xx", "yy", "zz", "yz", "xz", "xy"], {}
+#             for i, stress_field in enumerate(stress_fields): stress_fields_data[stress_field] = atoms.calc.results["stress"][i] * atoms.get_volume() * -1
+#             fileobj.write(" PlusStress:  " + "   ".join(stress_fields) + "\n")
+#             fileobj.write("    {} {} {} {} {} {}\n".format(fmt % stress_fields_data["xx"], fmt % stress_fields_data["yy"], fmt % stress_fields_data["zz"], fmt % stress_fields_data["yz"], fmt % stress_fields_data["xz"], fmt % stress_fields_data["xy"]))
+
+#         if "features" in atoms.info:
+#             for feature_name, feature_value in atoms.info["features"].items():
+#                 fileobj.write(" Feature    {} {}\n".format(feature_name, feature_value))
+
+#         fileobj.write("END_CFG\n")
+#         fileobj.write("\n")
+
 def write_cfg(fileobj, images, fmt='%12.6f'):
     def map2ranks(arr):
         types = []
@@ -72,14 +142,16 @@ def write_cfg(fileobj, images, fmt='%12.6f'):
         rank_map = {val: i for i, val in enumerate(types)}
         return [rank_map[num] for num in arr]
 
+    output = []
+
     for atoms in images:
-        fileobj.write("BEGIN_CFG\n")
-        fileobj.write(" Size\n")
-        fileobj.write("%9d\n" % len(atoms))
-        fileobj.write(" Supercell\n")
-        fileobj.write("    {} {} {}\n".format(fmt % atoms.get_cell()[0][0], fmt % atoms.get_cell()[0][1], fmt % atoms.get_cell()[0][2]))
-        fileobj.write("    {} {} {}\n".format(fmt % atoms.get_cell()[1][0], fmt % atoms.get_cell()[1][1], fmt % atoms.get_cell()[1][2]))
-        fileobj.write("    {} {} {}\n".format(fmt % atoms.get_cell()[2][0], fmt % atoms.get_cell()[2][1], fmt % atoms.get_cell()[2][2]))
+        output += ["BEGIN_CFG\n"]
+        output += [" Size\n"]
+        output += ["%9d\n" % len(atoms)]
+        output += [" Supercell\n"]
+        output += ["    {} {} {}\n".format(fmt % atoms.get_cell()[0][0], fmt % atoms.get_cell()[0][1], fmt % atoms.get_cell()[0][2])]
+        output += ["    {} {} {}\n".format(fmt % atoms.get_cell()[1][0], fmt % atoms.get_cell()[1][1], fmt % atoms.get_cell()[1][2])]
+        output += ["    {} {} {}\n".format(fmt % atoms.get_cell()[2][0], fmt % atoms.get_cell()[2][1], fmt % atoms.get_cell()[2][2])]
 
         fields, fields_data = ["id", "type", "cartes_x", "cartes_y", "cartes_z"], {"id":[], "type":[], "cartes_x":[], "cartes_y":[], "cartes_z":[]}
         if atoms.calc != None and "forces" in atoms.calc.results:
@@ -108,30 +180,33 @@ def write_cfg(fileobj, images, fmt='%12.6f'):
             else:
                 atoms.info["features"] = {"MV_grade": numpy.max(atoms.arrays["nbh_grades"])}
 
-        fileobj.write(" AtomData:  " + "    ".join(fields) + "\n")
+        output += [" AtomData:  " + "    ".join(fields) + "\n"]
         for i in range(len(atoms)):
-            fileobj.write(" {:9d} {:3d} {} {} {} ".format(fields_data["id"][i], fields_data["type"][i], fmt % fields_data["cartes_x"][i], fmt % fields_data["cartes_y"][i], fmt % fields_data["cartes_z"][i]))
+            line = " {:9d} {:3d} {} {} {} ".format(fields_data["id"][i], fields_data["type"][i], fmt % fields_data["cartes_x"][i], fmt % fields_data["cartes_y"][i], fmt % fields_data["cartes_z"][i])
             if atoms.calc != None and "forces" in atoms.calc.results:
-                fileobj.write(" {} {} {} ".format(fmt % fields_data["fx"][i], fmt % fields_data["fy"][i], fmt % fields_data["fz"][i]))
+                line += " {} {} {} ".format(fmt % fields_data["fx"][i], fmt % fields_data["fy"][i], fmt % fields_data["fz"][i])
             if "nbh_grades" in atoms.arrays:
-                fileobj.write(" {}".format(fmt % fields_data["nbh_grades"][i]))
-            fileobj.write("\n")
+                line += " {}".format(fmt % fields_data["nbh_grades"][i])
+            line += "\n"
+            output += [line]
 
-        fileobj.write(" Energy\n")
+        output += [" Energy\n"]
         if atoms.calc != None and "energy" in atoms.calc.results:
-            fileobj.write("    {}\n".format(fmt % atoms.calc.results["energy"]))
+            output += ["    {}\n".format(fmt % atoms.calc.results["energy"])]
         else:
-            fileobj.write("    {}\n".format(fmt % 0.0))
+            output += ["    {}\n".format(fmt % 0.0)]
 
         if atoms.calc != None and "stress" in atoms.calc.results:
             stress_fields, stress_fields_data = ["xx", "yy", "zz", "yz", "xz", "xy"], {}
             for i, stress_field in enumerate(stress_fields): stress_fields_data[stress_field] = atoms.calc.results["stress"][i] * atoms.get_volume() * -1
-            fileobj.write(" PlusStress:  " + "   ".join(stress_fields) + "\n")
-            fileobj.write("    {} {} {} {} {} {}\n".format(fmt % stress_fields_data["xx"], fmt % stress_fields_data["yy"], fmt % stress_fields_data["zz"], fmt % stress_fields_data["yz"], fmt % stress_fields_data["xz"], fmt % stress_fields_data["xy"]))
+            output += [" PlusStress:  " + "   ".join(stress_fields) + "\n"]
+            output += ["    {} {} {} {} {} {}\n".format(fmt % stress_fields_data["xx"], fmt % stress_fields_data["yy"], fmt % stress_fields_data["zz"], fmt % stress_fields_data["yz"], fmt % stress_fields_data["xz"], fmt % stress_fields_data["xy"])]
 
         if "features" in atoms.info:
             for feature_name, feature_value in atoms.info["features"].items():
-                fileobj.write(" Feature    {} {}\n".format(feature_name, feature_value))
+                output += [" Feature    {} {}\n".format(feature_name, feature_value)]
 
-        fileobj.write("END_CFG\n")
-        fileobj.write("\n")
+        output += ["END_CFG\n"]
+        output += ["\n"]
+
+    fileobj.write("".join(output))
